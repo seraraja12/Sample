73b7c406a7731d660ac05df0e49d3ddec725220e
sera <seraraja1196@gmail.com>
2025-11-28 12:55:52 +0530

    err

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -6,7 +6,7 @@ public class Sample {
 		
 		System.out.println("One");
 		System.out.println("Two");
-		System.out.println("Three");
+		System.err.println("Three");
 		System.out.println("Four");
 	}
 }
